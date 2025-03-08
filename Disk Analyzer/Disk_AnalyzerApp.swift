7d96e4ab7014ca3979efd71100965de57c3053d9
Ariesta App <ariesta.app@gmail.com>
2025-03-08 23:52:49 +0700

    Initial Commit

---
+++ Disk Analyzer/Disk_AnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disk_AnalyzerApp.swift
+//  Disk Analyzer
+//
+//  Created by Ariesta Agung on 08/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Disk_AnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
